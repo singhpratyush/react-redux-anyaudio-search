dbcab5f78251f5434655e004a09165a5aa86a482
Pratyush Singh <singh.pratyush96@gmail.com>
2017-02-19 04:36:52 +0530

    Added description about COSR limitation for server

--- aasearch/js/actions/search.jsx
+++ aasearch/js/actions/search.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default function fetchSearchResults(query) {
-    let url = 'http://127.0.0.1:5000/api/v1/search';
+    let url = 'http://127.0.0.1:5000/api/v1/search';  /*Host on local server and add CORS to concerned endpoint*/
     let response_obj = axios.get(url, {params: {q: query}});
     return {type: 'FETCH_RESULTS', payload: response_obj};
 }
